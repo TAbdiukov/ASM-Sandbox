9a66e525342a9125728912deaf34bb6400ede2a3
TAbdiukov <TAbdiukov@gmail.com>
2024-06-10 17:29:12 +0200

    Upload HW 1

---
+++ Win32-Hello-world.asm
@@ -0,0 +1,18 @@
+;!FASM
+
+; format binary
+format mz
+
+msg: db "Hello world$"
+
+jmp start
+
+start: 
+	mov ax, cs ; set Code Segment
+	mov ds, ax ; the same as Data Segment (break DEP)
+
+pre_echo: 
+	mov ax, 4c00h
+	mov dx, msg
+	
+do_echo: int 21h
