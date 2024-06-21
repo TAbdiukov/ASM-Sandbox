15a47e54b994618b1697a47660cda2390423b39e
TAbdiukov <TAbdiukov@gmail.com>
2024-06-21 03:06:51 +0200

    Bugfix

--- OS-loader-16bit.asm
+++ OS-loader-16bit.asm
@@ -3,31 +3,27 @@
 
 section .text ; KISS
 use16
-org 0x7c00 ; OEP is at 0x7c00
+bits 16
+org 0x7C00 ; EP is at 0x7C00
 
 start: 
 	mov ax, cs ; set Code Segment
 	mov ds, ax ; the same as Data Segment (break DEP)
 
-	mov si, msg ; message to be shown
-	cld ; ia-32 clear Direction flag (Delphi guidelines for assembler code)
-
-
-; jmp puts_loop
-
 pre_puts: ; prepare before puts()
+	mov si,amsg ; message to be shown
+	;cld ; ia-32 clear Direction flag (Delphi guidelines for assembler code)
 	mov ah, 0x0e ; AH=0Eh -> Teletype output
 	xor bh, bh ; mov bh, 0; Page number = 0 
 
-
 puts_loop: 
 	lodsb ; Like C + Python: `for c in *si: yield c` (into al)
-	test al, al ; is al Truthy, i.e. like in Python/Java `if(al): {}`; TEST is shorter than CMP.
-	jz interactive_fgets ; if not Truthy, exit loop to the next thing
+	test al,al ; is al Truthy, i.e. like in Python/Java `if(al): {}`; TEST is shorter than CMP.
+	jz puts_loop_out ; if not Truthy, exit loop to the next thing
 	int 10h ; interrupt vector 10h, AH=0Eh -> Teletype output
 	jmp puts_loop
 
-; jmp interactive_fgets
+puts_loop_out: jmp interactive_fgets
 
 interactive_fgets: ; loops forever
 	xor ah, ah ; mov ah, 0
@@ -38,16 +34,17 @@ interactive_fgets_backspace_after:
 	mov ah, 0x0e ; sanity check, this temporary information should change during a next key press
 	xor bh, bh ; mov bh, 0; Page number = 0
 	int 10h ; interrupt vector 10h, AH=0Eh -> Teletype output
+	jmp interactive_fgets ; jump back
 
 interactive_fgets_backspace: 
 mov al, 8 ; ASCII character of "Backspace" - remove character (supported by x86 BIOS)
 jmp interactive_fgets_backspace_after
 
 section .data
-msg db 'OS Startup message... ', 0
+  amsg db 'My OS loader...!', 13, 10, 0 ; 'TEXT\r\n\'
 
 ; Usage
-; 1. Compile in YASM into a .bin file
-; 2. In UltraISO, utilize .bin file as startup boot, same as .iso file
+; 1. Compile in YASM into a .bin/.ldr file (add extension if needed)
+; 2. In UltraISO, select .bin/ldr file and "Set Boot File", then save as .iso file
 ; 3. Use VirtualBox/VMWare in some basic Windows mode
 ; 4. Attach .iso file to the VM Instance
